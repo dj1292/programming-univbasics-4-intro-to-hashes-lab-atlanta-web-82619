7d1fe1224dc3afce7d918817a0b09938cfba1b86
Dan Reeves <djreeves1292@gmail.com>
2019-08-12 03:53:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,12 +34,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  i = 0 
- if hash[key] 
-   hash[key] += 1 
- end
- else 
-   hash[key] = 1 
+  if hash[key] 
+    hash[key] += 1
  end
  
    
